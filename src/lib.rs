f4be1ddf28c9bd3c26d2b44e24c355dba3d1598e
Manuel Quarneti <mq1@ik.me>
2026-04-05 11:37:29 +0200

    add format and region structs

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,166 @@
 
 use std::{
     borrow::Cow,
+    fmt,
     io::{self, Read},
 };
 
-const WII_MAGIC: [u8; 4] = [0x5D, 0x1C, 0x9E, 0xA3];
-const GC_MAGIC: [u8; 4] = [0xC2, 0x33, 0x9F, 0x3D];
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Format {
+    Iso,
+    Wbfs,
+    Ciso,
+    Rvz,
+    Wia,
+    Tgc,
+}
 
+impl Format {
+    pub fn initial_padding(self) -> Option<u64> {
+        match self {
+            Format::Wbfs => Some(0x200 - 0x6),
+            Format::Ciso | Format::Tgc => Some(0x8000 - 0x6),
+            Format::Rvz | Format::Wia => Some(0x58 - 0x6),
+            Format::Iso => None,
+        }
+    }
+}
+
+impl From<[u8; 4]> for Format {
+    fn from(magic: [u8; 4]) -> Self {
+        match magic {
+            [b'W', b'B', b'F', b'S'] => Self::Wbfs,
+            [b'C', b'I', b'S', b'O'] => Self::Ciso,
+            [b'R', b'V', b'Z', 0x01] => Self::Rvz,
+            [b'W', b'I', b'A', 0x01] => Self::Wia,
+            [0xae, 0x0f, 0x38, 0xa2] => Self::Tgc,
+            _ => Self::Iso,
+        }
+    }
+}
+
+impl fmt::Display for Format {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Iso => write!(f, "ISO"),
+            Self::Wbfs => write!(f, "WBFS"),
+            Self::Ciso => write!(f, "CISO"),
+            Self::Rvz => write!(f, "RVZ"),
+            Self::Wia => write!(f, "WIA"),
+            Self::Tgc => write!(f, "TGC"),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum RegionByte {
+    SystemWiiChannels,
+    UfouriaTheSagaNA,
+    Germany,
+    USA,
+    France,
+    NetherlandsEuropeAlternateLanguages,
+    Italy,
+    Japan,
+    Korea,
+    JapaneseImportToEuropeAustraliaAndOtherPALRegions,
+    AmericanImportToEuropeAustraliaAndOtherPALRegions,
+    JapaneseImportToUSAAndOtherNTSCRegions,
+    EuropeAndOtherPALRegionsSuchAsAustralia,
+    JapaneseVirtualConsoleImportToKorea,
+    Russia,
+    Spain,
+    AmericanVirtualConsoleImportToKorea,
+    AustraliaEuropeAlternateLanguages,
+    Scandinavia,
+    RepublicOfChinaTaiwanHongKongMacau,
+    EuropeAlternateLanguagesUSSpecialReleases,
+    Unknown(u8),
+}
+
+impl From<u8> for RegionByte {
+    fn from(byte: u8) -> Self {
+        match byte {
+            b'A' => Self::SystemWiiChannels,
+            b'B' => Self::UfouriaTheSagaNA,
+            b'D' => Self::Germany,
+            b'E' => Self::USA,
+            b'F' => Self::France,
+            b'H' => Self::NetherlandsEuropeAlternateLanguages,
+            b'I' => Self::Italy,
+            b'J' => Self::Japan,
+            b'K' => Self::Korea,
+            b'L' => Self::JapaneseImportToEuropeAustraliaAndOtherPALRegions,
+            b'M' => Self::AmericanImportToEuropeAustraliaAndOtherPALRegions,
+            b'N' => Self::JapaneseImportToUSAAndOtherNTSCRegions,
+            b'P' => Self::EuropeAndOtherPALRegionsSuchAsAustralia,
+            b'Q' => Self::JapaneseVirtualConsoleImportToKorea,
+            b'R' => Self::Russia,
+            b'S' => Self::Spain,
+            b'T' => Self::AmericanVirtualConsoleImportToKorea,
+            b'U' => Self::AustraliaEuropeAlternateLanguages,
+            b'V' => Self::Scandinavia,
+            b'W' => Self::RepublicOfChinaTaiwanHongKongMacau,
+            b'X' | b'Y' | b'Z' => Self::EuropeAlternateLanguagesUSSpecialReleases,
+            byte => Self::Unknown(byte),
+        }
+    }
+}
+
+impl fmt::Display for RegionByte {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::SystemWiiChannels => write!(f, "System Wii Channels (i.e. Mii Channel)"),
+            Self::UfouriaTheSagaNA => write!(f, "Ufouria: The Saga (NA)"),
+            Self::Germany => write!(f, "Germany"),
+            Self::USA => write!(f, "USA"),
+            Self::France => write!(f, "France"),
+            Self::NetherlandsEuropeAlternateLanguages => {
+                write!(f, "Netherlands / Europe alternate languages")
+            }
+            Self::Italy => write!(f, "Italy"),
+            Self::Japan => write!(f, "Japan"),
+            Self::Korea => write!(f, "Korea"),
+            Self::JapaneseImportToEuropeAustraliaAndOtherPALRegions => write!(
+                f,
+                "Japanese import to Europe, Australia and other PAL regions"
+            ),
+            Self::AmericanImportToEuropeAustraliaAndOtherPALRegions => write!(
+                f,
+                "American import to Europe, Australia and other PAL regions"
+            ),
+            Self::JapaneseImportToUSAAndOtherNTSCRegions => {
+                write!(f, "Japanese import to USA and other NTSC regions")
+            }
+            Self::EuropeAndOtherPALRegionsSuchAsAustralia => {
+                write!(f, "Europe and other PAL regions such as Australia")
+            }
+            Self::JapaneseVirtualConsoleImportToKorea => {
+                write!(f, "Japanese Virtual Console import to Korea")
+            }
+            Self::Russia => write!(f, "Russia"),
+            Self::Spain => write!(f, "Spain"),
+            Self::AmericanVirtualConsoleImportToKorea => {
+                write!(f, "American Virtual Console import to Korea")
+            }
+            Self::AustraliaEuropeAlternateLanguages => {
+                write!(f, "Australia / Europe alternate languages")
+            }
+            Self::Scandinavia => write!(f, "Scandinavia"),
+            Self::RepublicOfChinaTaiwanHongKongMacau => {
+                write!(f, "Republic of China (Taiwan) / Hong Kong / Macau")
+            }
+            Self::EuropeAlternateLanguagesUSSpecialReleases => {
+                write!(f, "Europe alternate languages / US special releases")
+            }
+            Self::Unknown(byte) => write!(f, "Unknown ({})", char::from(*byte)),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Meta {
+    format: Format,
     game_id: [u8; 6],
     disc_number: u8,
     disc_version: u8,
@@ -19,60 +172,22 @@ pub struct Meta {
 }
 
 impl Meta {
-    pub fn game_id(&self) -> Cow<'_, str> {
-        String::from_utf8_lossy(&self.game_id)
-    }
+    pub fn read<R: Read>(reader: &mut R) -> io::Result<Self> {
+        let mut game_id = [0; 6];
+        reader.read_exact(&mut game_id)?;
 
-    pub const fn region(&self) -> &'static str {
-        match self.game_id[3] {
-            b'A' => "System Wii Channels (i.e. Mii Channel)",
-            b'B' => "Ufouria: The Saga (NA)",
-            b'D' => "Germany",
-            b'E' => "USA",
-            b'F' => "France",
-            b'H' => "Netherlands / Europe alternate languages",
-            b'I' => "Italy",
-            b'J' => "Japan",
-            b'K' => "Korea",
-            b'L' => "Japanese import to Europe, Australia and other PAL regions",
-            b'M' => "American import to Europe, Australia and other PAL regions",
-            b'N' => "Japanese import to USA and other NTSC regions",
-            b'P' => "Europe and other PAL regions such as Australia",
-            b'Q' => "Japanese Virtual Console import to Korea",
-            b'R' => "Russia",
-            b'S' => "Spain",
-            b'T' => "American Virtual Console import to Korea",
-            b'U' => "Australia / Europe alternate languages",
-            b'V' => "Scandinavia",
-            b'W' => "Republic of China (Taiwan) / Hong Kong / Macau",
-            b'X' | b'Y' | b'Z' => "Europe alternate languages / US special releases",
-            _ => "Unknown",
+        let format = {
+            let mut buf = [0; 4];
+            buf.copy_from_slice(&game_id[..4]);
+            Format::from(buf)
+        };
+
+        if let Some(padding) = format.initial_padding() {
+            io::copy(&mut reader.take(padding), &mut io::sink())?;
+            let mut game_id = [0; 6];
+            reader.read_exact(&mut game_id)?;
         }
-    }
 
-    pub fn disc_number(&self) -> u8 {
-        self.disc_number
-    }
-
-    pub fn disc_version(&self) -> u8 {
-        self.disc_version
-    }
-
-    pub fn is_wii(&self) -> bool {
-        self.wii_magic == WII_MAGIC
-    }
-
-    pub fn is_gc(&self) -> bool {
-        self.gc_magic == GC_MAGIC
-    }
-
-    pub fn game_title(&self) -> Cow<'_, str> {
-        String::from_utf8_lossy(&self.game_title)
-    }
-}
-
-impl Meta {
-    pub fn read<R: Read>(reader: &mut R, game_id: [u8; 6]) -> io::Result<Self> {
         let disc_number = {
             let mut buf = [0; 1];
             reader.read_exact(&mut buf)?;
@@ -115,39 +230,46 @@ impl Meta {
             game_title,
         };
 
+        if !meta.is_wii() && !meta.is_gc() {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "Not a valid Wii or GameCube disc image",
+            ));
+        }
+
         Ok(meta)
     }
-}
 
-fn initial_padding(magic: &[u8]) -> Option<u64> {
-    match &magic[0..4] {
-        [b'W', b'B', b'F', b'S'] => Some(0x200 - 0x6),
-        [b'C', b'I', b'S', b'O'] | [0xae, 0x0f, 0x38, 0xa2] => Some(0x8000 - 0x6),
-        [b'R', b'V', b'Z', 0x01] | [b'W', b'I', b'A', 0x01] => Some(0x58 - 0x6),
-        _ => None,
-    }
-}
-
-/// Reads the disc header from a Wii/GameCube disc image (ISO or WBFS or CISO or RVZ or WIA or TGC)
-pub fn query<R: Read>(reader: &mut R) -> io::Result<Meta> {
-    let mut game_id = [0; 6];
-    reader.read_exact(&mut game_id)?;
-
-    let meta = if let Some(padding) = initial_padding(&game_id) {
-        io::copy(&mut reader.take(padding), &mut io::sink())?;
-        let mut game_id = [0; 6];
-        reader.read_exact(&mut game_id)?;
-        Meta::read(reader, game_id)?
-    } else {
-        Meta::read(reader, game_id)?
-    };
-
-    if !meta.is_wii() && !meta.is_gc() {
-        return Err(io::Error::new(
-            io::ErrorKind::InvalidData,
-            "Not a valid Wii or GameCube disc image",
-        ));
+    pub fn game_id(&self) -> Cow<'_, str> {
+        String::from_utf8_lossy(&self.game_id)
     }
 
-    Ok(meta)
+    pub fn region(&self) -> RegionByte {
+        // Ratatouille (RLWW78) has a region byte of 'W', but it's actually a Scandinavian release
+        if self.game_id == [b'R', b'L', b'W', b'W', b'7', b'8'] {
+            return RegionByte::Scandinavia;
+        }
+
+        self.game_id[3].into()
+    }
+
+    pub fn disc_number(&self) -> u8 {
+        self.disc_number
+    }
+
+    pub fn disc_version(&self) -> u8 {
+        self.disc_version
+    }
+
+    pub fn is_wii(&self) -> bool {
+        self.wii_magic == [0x5D, 0x1C, 0x9E, 0xA3]
+    }
+
+    pub fn is_gc(&self) -> bool {
+        self.gc_magic == [0xC2, 0x33, 0x9F, 0x3D]
+    }
+
+    pub fn game_title(&self) -> Cow<'_, str> {
+        String::from_utf8_lossy(&self.game_title)
+    }
 }
