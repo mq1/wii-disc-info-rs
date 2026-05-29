712f31c1d5b54ffbc78a2afb8557d5e687ca2ef5
Manuel Quarneti <mq1@ik.me>
2026-05-29 14:07:10 +0200

    Update gcz.rs

--- src/gcz.rs
+++ src/gcz.rs
@@ -4,36 +4,14 @@
 use crate::{Error, HEADER_SIZE};
 use std::io::{self, Read};
 
-fn read_u32le(slice: &[u8], offset: usize) -> u32 {
-    unsafe {
-        slice
-            .as_ptr()
-            .add(offset)
-            .cast::<u32>()
-            .read_unaligned()
-            .to_le()
-    }
-}
-
-fn read_u64le(slice: &[u8], offset: usize) -> u64 {
-    unsafe {
-        slice
-            .as_ptr()
-            .add(offset)
-            .cast::<u64>()
-            .read_unaligned()
-            .to_le()
-    }
-}
-
 pub fn read<R: Read>(reader: &mut R, header: &mut [u8; HEADER_SIZE]) -> Result<(), Error> {
-    let num_blocks = read_u32le(header, 0x1C);
+    let num_blocks = u32::from_le_bytes(header[0x1C..0x20].try_into().unwrap());
     if num_blocks == 0 {
         return Err(Error::Gcz);
     }
 
     // header[0x20..] contains the block pointer table inline
-    let blk0_ptr = read_u64le(header, 0x20);
+    let blk0_ptr = u64::from_le_bytes(header[0x20..0x28].try_into().unwrap());
     let blk0_offset = blk0_ptr & !(1u64 << 63);
     // Block 0 must be at offset 0 in the data region
     if blk0_offset != 0 {
@@ -43,10 +21,10 @@ pub fn read<R: Read>(reader: &mut R, header: &mut [u8; HEADER_SIZE]) -> Result<(
     // Determine compressed size of block 0
     let compressed_size = if num_blocks == 1 {
         // Use compressed_data_size field (header[0x08..0x10])
-        let compressed_data_size = read_u64le(header, 0x08);
+        let compressed_data_size = u64::from_le_bytes(header[0x08..0x10].try_into().unwrap());
         compressed_data_size as usize
     } else {
-        let blk1_ptr = read_u64le(header, 0x28);
+        let blk1_ptr = u64::from_le_bytes(header[0x28..0x30].try_into().unwrap());
         let blk1_offset = blk1_ptr & !(1u64 << 63);
         if blk1_offset == 0 {
             return Err(Error::Gcz);
@@ -58,7 +36,7 @@ pub fn read<R: Read>(reader: &mut R, header: &mut [u8; HEADER_SIZE]) -> Result<(
     // Reader is currently at byte HEADER_SIZE; data region starts at 0x20 + num_blocks * 12
     let data_start = 0x20 + num_blocks as u64 * 12;
     if HEADER_SIZE as u64 > data_start {
-        // this implies blocks are < 6 (highly likely to be corrupt)
+        // this also implies blocks are < 6 (highly likely to be corrupt)
         return Err(Error::Gcz);
     }
     let skip = data_start - HEADER_SIZE as u64;
